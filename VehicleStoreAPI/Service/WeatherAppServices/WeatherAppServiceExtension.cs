c09852c246ebd0bfa5ac43e02689073c56160082
Shubham Yadav <skychakkra@gmail.com>
2024-10-17 23:09:26 +0530

    Added Repository Pattern
    Added Repository Pattern.

---
+++ VehicleStoreAPI/Service/WeatherAppServices/WeatherAppServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿
+namespace VehicleStoreAPI.Service.WeatherAppServices
+{
+    public class WeatherAppServiceExtension : IWeatherAppService<WeatherForecast>
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> GetData()
+        {
+            return Enumerable.Range(1,30).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
